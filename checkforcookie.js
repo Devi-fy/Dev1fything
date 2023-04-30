19279f949d3f2ccff77935edd67a38bbd5e3c325
Devify <122911952+Devi-fy@users.noreply.github.com>
2023-04-30 22:02:46 +0000

    Update

---
+++ checkforcookie.js
@@ -0,0 +1,18 @@
+function getCookie(name){
+    var pattern = RegExp(name + "=.[^;]*")
+    var matched = document.cookie.match(pattern)
+    if(matched){
+        var cookie = matched[0].split('=')
+        return cookie[1]
+    }
+    return false
+};
+
+var cookie = getCookie("Checkforpass");
+
+if (cookie=="true") {
+    console.log("Allowed in")
+}
+else {
+    document.location.href="waitforpass.html";
+};
---
+++ checkforcookie2.js
@@ -0,0 +1,34 @@
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
+function getCookie(name){
+    var pattern = RegExp(name + "=.[^;]*")
+    var matched = document.cookie.match(pattern)
+    if(matched){
+        var cookie = matched[0].split('=')
+        return cookie[1]
+    }
+    return false
+};
+
+function start() {
+    var p = prompt("Please get the/a Owner/Admin to input the password... \nif you have a key please input it!")
+    if (p=="schoolistheworstthingevertoexist") {
+        console.log("Correct Password")
+        var key = makeid(length)
+        document.cookie = "Checkforpass=true; key=" + key;
+        alert("NEW KEY: " + key)
+    }
+    else if (p==getCookie("key")) {
+        alert("Correct!")
+    }
+};
