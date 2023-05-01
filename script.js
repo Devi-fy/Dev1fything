0e7b6c43327fc045b4ee6b1edf9a94473d8c1204
Devify <122911952+Devi-fy@users.noreply.github.com>
2023-05-01 23:21:16 +0000

    Update

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ document.querySelectorAll('.transition').forEach(link => {
 });
 
 function aboutblank() {
-  document.location.href = "/index"
+  document.location.href = "/Other"
   var url = prompt("Paste the link you want to be embedded into an about:blank page.", "https://example.com")
 ; var urlObj = new window.URL(window.location.href); win = window.open()
 ; win.document.body.style.margin = "0"; win.document.body.style.height = "100vh"; var iframe = win.document.createElement
